6a645bd1580c3c3c4a5bd343d9476e5de837b33f
Alan Dong <adong@yahoo-inc.com>
2017-07-05 17:14:24 -0700

    Create a page

---
+++ assets/dummy.js
@@ -0,0 +1,157 @@
+"use strict";
+
+
+
+define('dummy/app', ['exports', 'ember', 'dummy/resolver', 'ember-load-initializers', 'dummy/config/environment'], function (exports, _ember, _dummyResolver, _emberLoadInitializers, _dummyConfigEnvironment) {
+
+  var App = undefined;
+
+  _ember['default'].MODEL_FACTORY_INJECTIONS = true;
+
+  App = _ember['default'].Application.extend({
+    modulePrefix: _dummyConfigEnvironment['default'].modulePrefix,
+    podModulePrefix: _dummyConfigEnvironment['default'].podModulePrefix,
+    Resolver: _dummyResolver['default']
+  });
+
+  (0, _emberLoadInitializers['default'])(App, _dummyConfigEnvironment['default'].modulePrefix);
+
+  exports['default'] = App;
+});
+define('dummy/components/stupid-switch', ['exports', 'stupid-switch/components/stupid-switch'], function (exports, _stupidSwitchComponentsStupidSwitch) {
+  Object.defineProperty(exports, 'default', {
+    enumerable: true,
+    get: function get() {
+      return _stupidSwitchComponentsStupidSwitch['default'];
+    }
+  });
+});
+define('dummy/components/stupid-switches', ['exports', 'stupid-switch/components/stupid-switches'], function (exports, _stupidSwitchComponentsStupidSwitches) {
+  Object.defineProperty(exports, 'default', {
+    enumerable: true,
+    get: function get() {
+      return _stupidSwitchComponentsStupidSwitches['default'];
+    }
+  });
+});
+define('dummy/initializers/container-debug-adapter', ['exports', 'ember-resolver/container-debug-adapter'], function (exports, _emberResolverContainerDebugAdapter) {
+  exports['default'] = {
+    name: 'container-debug-adapter',
+
+    initialize: function initialize() {
+      var app = arguments[1] || arguments[0];
+
+      app.register('container-debug-adapter:main', _emberResolverContainerDebugAdapter['default']);
+      app.inject('container-debug-adapter:main', 'namespace', 'application:main');
+    }
+  };
+});
+define('dummy/initializers/export-application-global', ['exports', 'ember', 'dummy/config/environment'], function (exports, _ember, _dummyConfigEnvironment) {
+  exports.initialize = initialize;
+
+  function initialize() {
+    var application = arguments[1] || arguments[0];
+    if (_dummyConfigEnvironment['default'].exportApplicationGlobal !== false) {
+      var theGlobal;
+      if (typeof window !== 'undefined') {
+        theGlobal = window;
+      } else if (typeof global !== 'undefined') {
+        theGlobal = global;
+      } else if (typeof self !== 'undefined') {
+        theGlobal = self;
+      } else {
+        // no reasonable global, just bail
+        return;
+      }
+
+      var value = _dummyConfigEnvironment['default'].exportApplicationGlobal;
+      var globalName;
+
+      if (typeof value === 'string') {
+        globalName = value;
+      } else {
+        globalName = _ember['default'].String.classify(_dummyConfigEnvironment['default'].modulePrefix);
+      }
+
+      if (!theGlobal[globalName]) {
+        theGlobal[globalName] = application;
+
+        application.reopen({
+          willDestroy: function willDestroy() {
+            this._super.apply(this, arguments);
+            delete theGlobal[globalName];
+          }
+        });
+      }
+    }
+  }
+
+  exports['default'] = {
+    name: 'export-application-global',
+
+    initialize: initialize
+  };
+});
+define('dummy/resolver', ['exports', 'ember-resolver'], function (exports, _emberResolver) {
+  exports['default'] = _emberResolver['default'];
+});
+define('dummy/router', ['exports', 'ember', 'dummy/config/environment'], function (exports, _ember, _dummyConfigEnvironment) {
+
+  var Router = _ember['default'].Router.extend({
+    location: _dummyConfigEnvironment['default'].locationType,
+    rootURL: _dummyConfigEnvironment['default'].rootURL
+  });
+
+  Router.map(function () {});
+
+  exports['default'] = Router;
+});
+define('dummy/services/ajax', ['exports', 'ember-ajax/services/ajax'], function (exports, _emberAjaxServicesAjax) {
+  Object.defineProperty(exports, 'default', {
+    enumerable: true,
+    get: function get() {
+      return _emberAjaxServicesAjax['default'];
+    }
+  });
+});
+define('dummy/stupid-switch/tests/addon.lint-test', ['exports'], function (exports) {
+  QUnit.module('ESLint | addon');
+
+  QUnit.test('stupid-switch/components/stupid-switch.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'stupid-switch/components/stupid-switch.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('stupid-switch/components/stupid-switches.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'stupid-switch/components/stupid-switches.js should pass ESLint\n\n');
+  });
+});
+define("dummy/templates/application", ["exports"], function (exports) {
+  exports["default"] = Ember.HTMLBars.template({ "id": "T/ILgEsc", "block": "{\"statements\":[[\"append\",[\"unknown\",[\"stupid-switches\"]],false],[\"text\",\"\\n\\n\"],[\"append\",[\"unknown\",[\"outlet\"]],false],[\"text\",\"\\n\"]],\"locals\":[],\"named\":[],\"yields\":[],\"blocks\":[],\"hasPartials\":false}", "meta": { "moduleName": "dummy/templates/application.hbs" } });
+});
+
+
+define('dummy/config/environment', ['ember'], function(Ember) {
+  var prefix = 'dummy';
+try {
+  var metaName = prefix + '/config/environment';
+  var rawConfig = document.querySelector('meta[name="' + metaName + '"]').getAttribute('content');
+  var config = JSON.parse(unescape(rawConfig));
+
+  var exports = { 'default': config };
+
+  Object.defineProperty(exports, '__esModule', { value: true });
+
+  return exports;
+}
+catch(err) {
+  throw new Error('Could not read config from meta tag with name "' + metaName + '".');
+}
+
+});
+
+if (!runningTests) {
+  require("dummy/app")["default"].create({});
+}
+//# sourceMappingURL=dummy.map
